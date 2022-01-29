3d56ca2c0a5c7a4bd6b0782ca3c76dadc9b30bef
Rohith Yamsani <201801079@daiict.ac.in>
2022-01-29 12:13:53 +0530

    Creating Enrollment Form using Template Driven Forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
-
+import { User } from './user';
+import { EnrollmentService } from './enrollment.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'tdf';
+  topics = ['Angular', 'React', 'Vue']
+  topicHasError = true;
+  submitted = false;
+
+  userModel= new User('Rohith', 'rohith@gmail.com', 798173335, 'default', 'morning', true )
+
+  constructor(private _enrollmentService: EnrollmentService){}
+
+  validateTopic(value: string) {
+    if (value === 'default') {
+      this.topicHasError = true;
+    } else {
+      this.topicHasError = false;
+    }
+  }
+
+  onSubmit(){
+    this.submitted = true;
+    this._enrollmentService.enroll(this.userModel)
+    .subscribe(
+      data => console.log('Success!', data),
+      error => console.log('Error', error)
+      
+    )
+  }
 }
